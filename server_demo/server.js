dea53167afe4b43ac0c117cbf3f151d7cd7cd0a1
Navraj Kalsi <navrajkalsi@icloud.com>
2025-06-16 21:17:49 -0400

    Added vim motions in the directory html file

--- server_demo/server.js
+++ server_demo/server.js
@@ -1,11 +1,67 @@
 const files_list = document.getElementsByTagName("ul")[0],
   current_url = window.location.href,
-  current_path = new URL(current_url).pathname;
+  current_path = new URL(current_url).pathname,
+  file_preview = document.getElementById("file-preview");
 
-document.getElementById("current_url").textContent = current_path;
+// This is the 'li' element which would be selected
+var selected_file = null;
 
-for (const file of files_list.children) {
-  file.onclick = () => {
-    window.location.href = current_url + '/' + file.textContent;
-  };
+function show_file() {
+  if (selected_file == null)
+    return;
+  if (selected_file.textContent.endsWith("/"))
+    return;
+
+  fetch(current_path + "/" + selected_file.textContent)
+    .then(async response => {
+      file_preview.innerHTML = await response.text();
+    });
+};
+
+function select_file(to_select) {
+  if (to_select == null)
+    return;
+
+  if (selected_file)
+    selected_file.classList.remove("highlight");
+  to_select.classList.add("highlight");
+  selected_file = to_select;
+
+  show_file();
 }
+
+// Hanldes motions and keypresses to change the highlighted file
+function handle_motion() {
+  document.onkeyup = (event) => {
+    if (event.key == "j" || event.key == "Down" || event.key == "ArrowDown")
+      select_file(selected_file.nextElementSibling);
+    if (event.key == "k" || event.key == "Up" || event.key == "ArrowUp")
+      select_file(selected_file.previousElementSibling);
+    if (event.key == "G")
+      select_file(files_list.lastElementChild);
+    if (event.key == "g")
+      select_file(files_list.firstElementChild);
+    if (event.key == "Enter") {
+      var file_name = selected_file.textContent;
+      if (file_name.endsWith("/"))
+        window.location.href = current_url + "/" + file_name.substring(0, file_name.length - 1);
+      else
+        window.location.href = current_url + "/" + file_name;
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("current_url").textContent = current_path;
+
+  for (const file of files_list.children) {
+    file.onclick = () => {
+      window.location.href = current_url + '/' + file.textContent;
+    };
+  }
+
+  // Highlighting the first file
+  select_file(files_list.firstElementChild);
+
+  handle_motion();
+});
--- server_demo/server_explain.c
+++ server_demo/server_explain.c
@@ -24,6 +24,8 @@
 #define READ_BUFFER_SIZE 4096
 #define METHOD_SIZE 10
 #define PATH_SIZE 4096
+// Response related
+#define STATUS_SIZE 32
 
 // Variable to determine running status of server
 volatile sig_atomic_t running = 1;
@@ -43,6 +45,7 @@ struct client_info {
   char request_path[PATH_SIZE];
   char *response;
   unsigned int response_len;
+  char response_status[STATUS_SIZE];
 };
 
 // Main server socket file descriptor
@@ -83,44 +86,62 @@ int set_root_dir(void) {
   return 0;
 }
 
+// Handles requesting of any static files like favicon
+// Returns 1 if the path has to be dealt with statically and not to be used with
+// realpath() in parse_request()
+int check_static_request(struct client_info *client) {
+  if (!client)
+    return -1;
+
+  if (strcmp(client->request_path, "/favicon.ico") == 0)
+    return 1;
+  return 0;
+}
+
 // Parses a request, extracting the 'request_method' & 'request_path'.
 // Request path is converted to absolute path and checked for traversal
-int parse_request(const char *request, char *request_method,
-                  char *request_path) {
+int parse_request(struct client_info *client) {
   // Change safe limits here based on max method and path sizes
   // 1 less because of null pointer
   // Also change the number of returned args in case more args are required in
   // future
-  printf("Received Request: \n\n%s", request);
-  if (sscanf(request, "%9s %4095s", request_method, request_path) != 2)
+  printf("Received Request: \n\n%s", client->read_buffer);
+  if (sscanf(client->read_buffer, "%9s %4095s", client->request_method,
+             client->request_path) != 2)
     return -1;
 
-  // Thanks Prof Kevin Forest!
-  // Skipping the beginning '/' by shifting the pointer to the next char
-  if (request_path[0] == '/')
-    request_path += 1;
+  int is_path_static = check_static_request(client);
 
-  // Getting actual absolute path of the target
-  if (!realpath(request_path, request_path)) {
-    // If the errno is set to 2, that means the requested directory/file does
-    // not exist Then the server serves the '404.html file instead'
-    if (errno == 2) {
-      if (!realpath(
-              "404.html",
-              request_path)) // If the file does not exist, request path is set
-                             // to the absolute path of 404.html file
+  if (is_path_static == 0) { // Absolute path is to be used
+    // Thanks Prof Kevin Forest!
+    // Skipping the beginning '/' by shifting the pointer to the next char
+    // Getting actual absolute path of the target
+    if (!realpath(&(client->request_path)[1], client->request_path)) {
+      // If the errno is set to 2, that means the requested directory/file does
+      // not exist Then the server serves the '404.html file instead'
+      if (errno == 2) {
+        snprintf(client->response_status, STATUS_SIZE,
+                 "404 Not Found"); // Setting status code
+        if (!realpath("404.html",
+                      client->request_path)) // If the file does not exist,
+                                             // request path is set to the
+                                             // absolute path of 404.html file
+          return -1;
+      } else
         return -1;
+    }
 
-    } else
+    // Comparing the starting chars of the 'root_dir' and 'request_path' to
+    // prevent any path traversal
+    if (strncmp(root_dir, client->request_path, strlen(root_dir)) != 0) {
+      errno = EPERM;
       return -1;
-  }
-
-  // Comparing the starting chars of the 'root_dir' and 'request_path' to
-  // prevent any path traversal
-  if (strncmp(root_dir, request_path, strlen(root_dir)) != 0) {
-    errno = EPERM;
+    }
+  } else if (is_path_static == -1)
     return -1;
-  }
+  else // File is static and needs to be served from the server path. Removing
+       // the starting '/'
+    strncpy(client->request_path, &(client->request_path)[1], PATH_SIZE);
 
   return 0;
 }
@@ -136,16 +157,20 @@ const char *get_mime_type(const char *filepath) {
   }
 
   const char *mime = magic_file(magic, filepath);
+  magic_close(magic);
+
   return mime;
 }
 
 // Generates an HTTP response header
-int generate_header(char **header, const char *status, const char *content_type,
+int generate_header(char **header, char *status, const char *content_type,
                     unsigned int content_length, unsigned int *header_size) {
+  if (status[0] == '\0')
+    snprintf(status, STATUS_SIZE, "200 OK");
 
-  const char *header_template =
-      "HTTP/1.1 %s\r\n Content-Type: %s\r\n Content-Length: %u\r\n Connection: "
-      "close\r\n\r\n";
+  const char *header_template = "HTTP/1.1 %s\r\n Content-Type: %s\r\n "
+                                "Content-Length: %u\r\n Connection: "
+                                "close\r\n\r\n";
 
   int final_len = snprintf(NULL, 0, header_template, status, content_type,
                            content_length); // calculating just the final length
@@ -154,10 +179,12 @@ int generate_header(char **header, const char *status, const char *content_type,
     errno = ENOMEM;
     return -1;
   }
+
   // Actually adding the response header
   snprintf(*header, final_len + 1, header_template, status, content_type,
            content_length);
   *header_size = final_len;
+
   return 0;
 }
 
@@ -166,9 +193,13 @@ int generate_header(char **header, const char *status, const char *content_type,
 // Content type and full HTTP header is set here
 // 'size' is an output variable that is used to output the final size of the
 // response
-int read_file(const char *request_path, char **response, unsigned int *size) {
+int read_file(struct client_info *client, const char *alternate_path) {
 
-  FILE *file = fopen(request_path, "r");
+  FILE *file;
+  if (alternate_path == NULL)
+    file = fopen(client->request_path, "r");
+  else
+    file = fopen(alternate_path, "r");
 
   if (file == NULL)
     return -1;
@@ -182,39 +213,52 @@ int read_file(const char *request_path, char **response, unsigned int *size) {
   // Final content length will be 'pos' when just serving a file
   // Final content length will be 'pos + *size' showing a directory, *size at
   // this point is just the size of 'dirs'
-  if (generate_header(&header, "200 OK", get_mime_type(request_path),
-                      pos + *size, &header_size) == -1)
-    return -1;
+  if (alternate_path == NULL) {
+    if (generate_header(&header, client->response_status,
+                        get_mime_type(client->request_path),
+                        pos + client->response_len, &header_size) == -1) {
+      fclose(file);
+      return -1;
+    }
+  } else {
+    if (generate_header(&header, client->response_status,
+                        get_mime_type(alternate_path),
+                        pos + client->response_len, &header_size) == -1) {
+      fclose(file);
+      return -1;
+    }
+  }
 
   // Response buffer with null-terminator
-  *response = (char *)malloc(pos + header_size);
-  *response[0] = '\0';
+  client->response = (char *)malloc(pos + header_size);
+  client->response[0] = '\0';
 
-  if (!*response) {
+  if (!(client->response)) {
     fclose(file);
     errno = ENOMEM; // Errno for malloc errors
     return -1;
   }
 
   // Adding header to the beginning of the response
-  strcat(*response, header);
+  strcat(client->response, header);
 
   // Number of bytes read, used as size of response
   // Reading to the end of header index
-  *size += fread(&((*response)[header_size]), sizeof(char), pos, file);
-  *size += header_size;
+  client->response_len +=
+      fread(&((client->response)[header_size]), sizeof(char), pos, file);
+  client->response_len += header_size;
   // Lesson learned
-  (*response)[*size] = '\0';
+  (client->response)[client->response_len] = '\0';
 
   fclose(file);
   free(header);
   return 0;
-};
+}
 
-// Reads the server.html file and fills the response buffer with it, then edits
-// the buffer to insert the contents of the directory into it.
-int read_directory(const char *request_path, char **response,
-                   unsigned int *size) {
+// Reads the server.html file and fills the response buffer with it, then
+// edits the buffer to insert the contents of the directory into it.
+int read_directory(struct client_info *client) {
+
   // Single directory entry
   struct dirent *dir_entry;
   DIR *dir_ptr;
@@ -224,36 +268,42 @@ int read_directory(const char *request_path, char **response,
   unsigned int dirs_size = 0;
 
   // Actual directory pointer
-  if ((dir_ptr = opendir(request_path))) {
+  if ((dir_ptr = opendir(client->request_path))) {
     while ((dir_entry = readdir(dir_ptr)) != NULL) {
       // Name of every dir_entry
       char *file_name = dir_entry->d_name;
       // Not adding back and current directories
       if (strcmp(file_name, "..") == 0 || strcmp(file_name, ".") == 0)
         continue;
+
       dirs_size += strlen(file_name) + strlen("<li>") + strlen("</li>\n");
       strcat(dirs, "<li>");
       strcat(dirs, file_name);
+
+      // Appending a '/' if the entry is a directory itself
+      if (dir_entry->d_type == DT_DIR)
+        strcat(dirs, "/");
       strcat(dirs, "</li>\n");
     }
   } else
     return -1;
 
   // New size of response
-  *size += dirs_size;
+  client->response_len += dirs_size;
 
   // Filling the response buffer with the server.html template
   // File only needs to be read here, since it has to be sent the new size,
   // including 'dirs'
-  if (read_file("./server.html", response, size) == -1)
+  if (read_file(client, "./server.html") == -1)
     return -1;
 
-  char *new_response = malloc(*size);
+  char *new_response = malloc(client->response_len);
 
   // Finding ~ in the html file
   int mark_index;
-  for (mark_index = 0; mark_index < *size - dirs_size; ++mark_index) {
-    if ((*response)[mark_index] == '~')
+  for (mark_index = 0; mark_index < client->response_len - dirs_size;
+       ++mark_index) {
+    if ((client->response)[mark_index] == '~')
       break;
   }
   if (!mark_index) {
@@ -262,36 +312,35 @@ int read_directory(const char *request_path, char **response,
   }
 
   // Copying first part into the new response and concatenating dirs
-  strncpy(new_response, *response, mark_index);
+  strncpy(new_response, client->response, mark_index);
   // Not null terminated, terminating to use strcat
   new_response[mark_index] = '\0';
   strcat(new_response, dirs);
   // Appending the remaining file
-  strcat(new_response, *response + mark_index + 1);
+  strcat(new_response, client->response + mark_index + 1);
 
-  free(*response);
-  *response = new_response;
+  free(client->response);
+  client->response = new_response;
   closedir(dir_ptr);
   return 0;
-};
+}
 
 // Takes in path, checks if it points to a directory or file, call the
 // respective functions to fill the response and sets size of the response
 // buffer Pointer to reponse pointer is required to change the response in the
 // main function
-int generate_response(const char *request_path, char **response,
-                      unsigned int *size) {
+int generate_response(struct client_info *client) {
 
   // Metadata of the dir/file
   struct stat request_path_stat;
 
-  if (stat(request_path, &request_path_stat) == -1)
+  if (stat(client->request_path, &request_path_stat) == -1)
     return -1;
   else if (S_ISREG(request_path_stat.st_mode)) { // File
-    if (read_file(request_path, response, size) == -1)
+    if (read_file(client, NULL) == -1)
       return -1;
   } else if (S_ISDIR(request_path_stat.st_mode)) { // Directory
-    if (read_directory(request_path, response, size) == -1)
+    if (read_directory(client) == -1)
       return -1;
   } else {
     errno = EIO;
@@ -311,17 +360,16 @@ int main(void) {
   // sockets & UDP for datagram sockets)
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-  // Now the socket has to be binded to an IP & a port, the address should be of
-  // any one of the interfaces on this machine. After binding all request to
-  // this socket will be routed to that particular IP/port. The address can be:
-  // INADDR_ANY: it is used to target all the avaliable interfaces on the
+  // Now the socket has to be binded to an IP & a port, the address should be
+  // of any one of the interfaces on this machine. After binding all request
+  // to this socket will be routed to that particular IP/port. The address can
+  // be: INADDR_ANY: it is used to target all the avaliable interfaces on the
   // machine. Localhost: to target localhost, have to use the loopback address
   // (127.0.0.1) Any IP on the network, also have to convert it to binary form
   // :: is used to bind to all IPv6 addresses.
-  // Note: Cannot just assign a string IP address to the server, have to convert
-  // it to binary with inet_pton()
-  // Also the port has to be  a short in network byte order(big endian), with
-  // htons()
+  // Note: Cannot just assign a string IP address to the server, have to
+  // convert it to binary with inet_pton() Also the port has to be  a short in
+  // network byte order(big endian), with htons()
   server_address.sin_family = AF_INET;
   server_address.sin_port = htons(PORT);
   server_address.sin_addr.s_addr = INADDR_ANY;
@@ -357,9 +405,9 @@ int main(void) {
     printf("Server Running at Port: %d\n", PORT);
     // Accepting connections, requires the server_fd and an empty 'struct
     // sockaddr' and its length. On connection, fills it with the address info
-    // of the client After accepting, all communication with the said client is
-    // done on the new client_fd and server_fd still remains open listening for
-    // new conenctions.
+    // of the client After accepting, all communication with the said client
+    // is done on the new client_fd and server_fd still remains open listening
+    // for new conenctions.
     struct client_info new_client;
 
     new_client.client_fd =
@@ -384,19 +432,20 @@ int main(void) {
         err_n_die("Reading");
       (new_client.read_buffer)[bytes_read] = '\0';
 
+      // Clearing any previous status codes
+      memset(new_client.response_status, 0, STATUS_SIZE);
+      (new_client.response_status)[0] = '\0';
       memset(root_dir, 0, PATH_SIZE);
       if (set_root_dir() == -1)
         err_n_die("Setting Root Directory");
 
-      if (parse_request(new_client.read_buffer, new_client.request_method,
-                        new_client.request_path) == -1)
+      if (parse_request(&new_client) == -1)
         err_n_die("Parsing Request");
 
       if (strncmp(new_client.request_method, "GET", 3) != 0)
         err_n_die("Request Method");
 
-      if (generate_response(new_client.request_path, &new_client.response,
-                            &new_client.response_len) == -1)
+      if (generate_response(&new_client) == -1)
         err_n_die("Generating Response");
 
       if (write(new_client.client_fd, new_client.response,
